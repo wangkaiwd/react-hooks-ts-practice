85b00e497dc244579f81a4f3091647db367196fe
wangkaiwd <1162486176@qq.com>
2019-11-18 22:16:02 +0800

    feat: some annotation of class methods understand

--- src/views/classVsHooks/ClassVsHooks.tsx
+++ src/views/classVsHooks/ClassVsHooks.tsx
@@ -32,6 +32,9 @@ class ClassComponents extends React.Component<Props, State> {
 
   // javascript 新的class属性写法，当属性为函数时可以这样写
   // @see: https://github.com/tc39/proposal-class-fields
+  // 这样写并没有将对应的方法写到原型上，而是作为class构建出来的实例的属性存在
+  // 原型上的方法是共有的，并不会在每次使用new创建实例的时候都进行绑定。而自身属性在每次创建实例的时候都需要添加，会浪费内存
+  // 有关属性方法，静态方法，原型方法的讨论：https://stackoverflow.com/a/1635143/11720536
   onClick = () => {
     console.log(this.state.n);
   };
