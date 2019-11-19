cb497317e04ddd00cd6d35470dec366eba23c961
wangkaiwd <1162486176@qq.com>
2019-11-19 23:03:21 +0800

    feat: first step to implement self useState

--- src/views/myUseState/myUseState.tsx
+++ src/views/myUseState/myUseState.tsx
@@ -1,7 +1,18 @@
-import React, { useState } from 'react';
-
+import React from 'react';
+import App from '@/App';
+import ReactDOM from 'react-dom';
+// @see: https://stackoverflow.com/a/45576880/11720536
+// 这里的返回值要使用元祖tuple来进行定义
+const myUseState = <T extends unknown> (initialValue: T): [T, (newValue: T) => void] => {
+  let n = initialValue;
+  const setN = (newValue: T): void => {
+    n = newValue;
+    ReactDOM.render(<App/>, document.getElementById('root'));
+  };
+  return [n, setN];
+};
 const Counter = () => {
-  const [n, setN] = useState(0);
+  const [n, setN] = myUseState(0);
   return (
     <div>
       {n}
