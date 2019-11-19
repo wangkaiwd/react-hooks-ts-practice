06f076ea635d6c512c459961b82d91f74f9bff11
wangkaiwd <1162486176@qq.com>
2019-11-19 23:46:24 +0800

    feat: implement a myUseState can use

--- src/views/myUseState/myUseState.tsx
+++ src/views/myUseState/myUseState.tsx
@@ -3,24 +3,32 @@ import App from '@/App';
 import ReactDOM from 'react-dom';
 // @see: https://stackoverflow.com/a/45576880/11720536
 // 这里的返回值要使用元祖tuple来进行定义
-let _state: any;
+let _state: any[] = [];
+let _index = 0;
 const myUseState = <T extends unknown> (initialValue: T): [T, (newValue: T) => void] => {
-  let n = typeof _state === 'undefined' ? initialValue : _state;
-  const setN = (newValue: T): void => {
-    n = newValue;
-    _state = newValue;
+  const currentIndex = _index;
+  let state = typeof _state[currentIndex] === 'undefined' ? initialValue : _state[currentIndex];
+  const setState = (newValue: T): void => {
+    _state[currentIndex] = newValue;
     ReactDOM.render(<App/>, document.getElementById('root'));
+    _index = 0;
   };
-  return [n, setN];
+  _index++;
+  return [state, setState];
 };
 const Counter = () => {
   // setN会重新渲染Counter,然后会重新执行myUseState
   const [n, setN] = myUseState(0);
-  console.log('component', n);
+
+  // 要分别区分不同的元素，这里根据索引来放入数组中标记不同的state值，所以hooks的顺序很重要
+  const [m, setM] = myUseState(0);
   return (
     <div>
       {n}
       <button onClick={() => setN(n + 1)}>click</button>
+      <hr/>
+      {m}
+      <button onClick={() => setM(m + 1)}>click</button>
     </div>
   );
 };
