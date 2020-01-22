e11b10d9703ddbfd61b0179ab7695c5bc5712804
wangkaiwd <1162486176@qq.com>
2020-01-22 10:48:39 +0800

    feat: second step of article study

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const App: React.FC = () => {
   const [data, setData] = useState<{ hits: any[] }>({ hits: [] });
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios('https://hn.algolia.com/api/v1/search?query=react');
+      setData(result.data);
+    };
+    fetchData().then();
+  }, []);
   return (
     <ul>
       {data.hits.map(item => (
