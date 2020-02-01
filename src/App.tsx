0d181ff8ca295f59dd1a0a3ee2a94765353808df
wangkaiwd <1162486176@qq.com>
2020-02-01 12:55:33 +0800

    feat: fetch data depend on another variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Card, List } from 'antd';
+import { Button, Card, Input, List } from 'antd';
 import { IData } from '@/responseTypes';
 
 const App: React.FC = () => {
   const [data, setData] = useState<IData>({ hits: [] });
+  const [query, setQuery] = useState('redux');
+  const [search, setSearch] = useState('redux');
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios('https://hn.algolia.com/api/v1/search?query=react');
+      const result = await axios(`https://hn.algolia.com/api/v1/search?query=${search}`);
       setData({ hits: result.data.hits });
     };
     fetchData().then();
-  }, []);
+  }, [search]);
   return (
     <Card bordered={false}>
+      <Input
+        type="text"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <Button onClick={() => setSearch(query)}>Search</Button>
       <List dataSource={data.hits} renderItem={(item) => (
         <List.Item>
           <a href={item.url}>{item.title}</a>
